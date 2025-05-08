c588f6d59737f2f9f03fdcf67fd6151df35f1229
M2.128 <mp1@emg.im>
2025-05-08 19:20:48 +0300

    Create сосал.py

---
+++ сосал.py
@@ -0,0 +1,7 @@
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+print("маму любишь?")
+if input() == "Да":
+    os.system('cls' if os.name == 'nt' else 'clear')
+print("Сосал?")
+print("Да")
